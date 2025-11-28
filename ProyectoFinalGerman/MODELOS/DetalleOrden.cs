3f85c1d79fa2cf62c6682eb6ec5c1f37f44a4d06
FredyGarcia09 <s23120180@alumnos.itsur.edu.mx>
2025-11-27 22:03:12 -0600

    Definición de los modelos

---
+++ ProyectoFinalGerman/MODELOS/DetalleOrden.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalGerman.MODELOS
+{
+    public class DetalleOrden
+    {
+        public int IdDetalleOrden { get; set; }
+        public int IdOrden { get; set; }
+        public int IdProducto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal Importe { get; set; } // Columna calculada 
+    }
+}
---
+++ ProyectoFinalGerman/MODELOS/ITEMS/ReporteCorporativoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalGerman.MODELOS.ITEMS
+{
+    public class ReporteComparativoItem
+    {
+        public int Id { get; set; }
+        public string Producto { get; set; }
+        public decimal Precio { get; set; }
+        public decimal VentasMes1 { get; set; } 
+        public decimal VentasMes2 { get; set; } 
+    }
+}
