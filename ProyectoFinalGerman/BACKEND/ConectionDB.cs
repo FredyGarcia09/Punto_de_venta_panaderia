b6609f0051a9ec983284cf6216b3c4e6a58ddb4e
FredyGarcia09 <s23120180@alumnos.itsur.edu.mx>
2025-11-27 18:50:36 -0600

    Primera definicion del backend y modelos

---
+++ ProyectoFinalGerman/BACKEND/ConectionDB.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalGerman.BACKEND
+{
+    public class ConexionDB
+    {
+        private readonly string connectionString = "Server=localhost;Database=VENTAS;Uid=root;Pwd=root;Port=3306;";
+
+        public MySqlConnection ObtenerConexion()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public bool ProbarConexion()
+        {
+            using (MySqlConnection conn = ObtenerConexion())
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error de conexión: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+    }
+}
